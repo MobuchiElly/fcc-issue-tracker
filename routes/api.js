851328e5b13604e184a6d403b7dbab54cbed70e6
MobuchiElly <ugwueleazer@gmail.com>
2025-03-09 15:19:04 +0100

    done implementing api routes for post, get, delete and put

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,7 @@ module.exports = function (app) {
       if (created_on) filterObj["created_on"] = new Date(created_on)
       if (updated_on) filterObj["updated_on"] = new Date(updated_on)
       
-      try { //let g ={ "created_on": new Date("2025-03-08T00:04:04.708Z") };
+      try {
         const db = await getDB();
         const issuesRes = await db.collection(project).find(filterObj).toArray();
         if(!issuesRes.length) return res.status(404).json("No issues in database");
@@ -65,36 +65,48 @@ module.exports = function (app) {
     
     .put(async function (req, res){
       let project = req.params.project;
-      const _id = req.body._id;
+      const { _id, issue_title, issue_text, created_by, assigned_to, status_text, open } = req.body;
       const updateData = {}
-      for (let key in req.body){
-        if (req.body[key] && key != "_id"){
-          //updateData[key] = {$regex: req.body[key], $options: "i"}
-          updateData[key] = req.body[key]
-          // if(key == "open" && key !== undefined){
 
-          // } else {
-
-          // }
-        }
-      }
-      console.log("updateData: ", updateData);
       if(!_id) return res.json({ error: 'missing _id' });
-      if(!Object.keys(updateData).length) return res.json({ error: 'no update field(s) sent', '_id': _id })
+
+      const issueId = new ObjectId(_id);
+      if (issue_title) updateData["issue_title"] = { $regex: issue_title, $options: "i"} 
+      if (issue_text) updateData["issue_text"] = { $regex: issue_text, $options: "i"}
+      if (created_by) updateData["created_by"] = { $regex: created_by, $options: "i"}
+      if (assigned_to) updateData["assigned_to"] = { $regex: assigned_to, $options: "i"}
+      if (status_text) updateData["status_text"] = { $regex: status_text, $options: "i"}
+      if (open) updateData["open"] = JSON.parse(open);
+      updateData["updated_on"] = new Date();
+
+      if(Object.keys(updateData).length === 1) return res.json({ error: 'no update field(s) sent', '_id': _id })
       try{
         const db = await getDB();
-        // const issueRes = await db.collection(project).UpdateOne({_id: new ObjectId(_id)}, {$set: updateData});
-        const issueRes = await db.collection(project).find({_id: new ObjectId("67cd8f49dcf472ab031706ac")}).toArray();
+        const issueRes = await db.collection(project).updateOne({_id: issueId}, {$set: updateData});
+        //const issueRes = await db.collection(project).find({_id: new ObjectId("67cd8f49dcf472ab031706ac")}).toArray();
         console.log("response:",issueRes);
+        if (issueRes.modifiedCount == 0) return res.json({ error: 'could not update', '_id': _id })
+        
         return res.json({  result: 'successfully updated', '_id': _id })
       } catch(err){
         return res.json({ error: 'could not update', '_id': _id });
       }
     })
     
-    .delete(function (req, res){
+    .delete(async function (req, res){
       let project = req.params.project;
-      
+      const {_id} = req.body;
+      if (!_id) return res.json({ error: 'missing _id' })
+      const issueId = new ObjectId(_id);
+
+      try {
+        const db = await getDB();
+        const response = db.collection(project).delete({_id: issueId});
+        if (response.deletedCount == 0) return res.json({ error: 'could not delete', '_id': _id })
+        return res.json({ result: 'successfully deleted', '_id': _id })
+      } catch(err){
+        return res.json({ error: 'could not delete', '_id': _id });
+      }
     });
     
 };
