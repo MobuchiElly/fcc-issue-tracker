f3f19cea0b1aa7b749937e10430d7802318ebde4
MobuchiElly <ugwueleazer@gmail.com>
2025-03-09 14:44:16 +0100

    fix

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,35 @@
 'use strict';
 
+const { ObjectId } = require("mongodb");
 const { getDB } = require("../connection");
 
 module.exports = function (app) {
-
-  // res  json: {"assigned_to":"buchdev","status_text":"pending","open":true,"_id":"67caa6fec899b20013513057","issue_title":"functional tests not working","issue_text":"functional tests not working","created_by":"buchi","created_on":"2025-03-07T07:57:50.015Z","updated_on":"2025-03-07T07:57:50.015Z"}
   
   app.route('/api/issues/:project')
   
-    .get(function (req, res){
+    .get(async function (req, res){
       let project = req.params.project;
-      console.log(req.params)
-      return res.send("tracking")
+      const { issue_title, issue_text, created_by, assigned_to, status_text, _id, open, created_on, updated_on } = req.query
+      let filterObj = {};
+
+      if (issue_title) filterObj["issue_title"] = {$regex: issue_title, $options: "i"}
+      if (issue_text) filterObj["issue_text"] = {$regex: issue_text, $options: "i"}
+      if (created_by) filterObj["created_by"] = {$regex: created_by, $options: "i"}
+      if (assigned_to) filterObj["assigned_to"] = {$regex: assigned_to, $options: "i"}
+      if (status_text) filterObj["status_text"] = {$regex: status_text, $options: "i"}
+      if (open) filterObj["open"] = JSON.parse(open)
+      if (_id) filterObj["_id"] = new ObjectId(_id)
+      if (created_on) filterObj["created_on"] = new Date(created_on)
+      if (updated_on) filterObj["updated_on"] = new Date(updated_on)
+      
+      try { //let g ={ "created_on": new Date("2025-03-08T00:04:04.708Z") };
+        const db = await getDB();
+        const issuesRes = await db.collection(project).find(filterObj).toArray();
+        if(!issuesRes.length) return res.status(404).json("No issues in database");
+        return res.status(200).json(issuesRes);
+      } catch(err){
+        res.status(500).json("server error");
+      }
     })
     
     .post(async function (req, res){
@@ -22,11 +40,10 @@ module.exports = function (app) {
       const assigned_to = req.body.assigned_to || '';
       const status_text = req.body.status_text || '';
       const created_by = req.body.created_by;
-
+      
       if(!issue_title || !issue_text || !created_by){
-        return res.status(400).json({ error: 'required field(s) missing' });
-      }
-
+        return res.json({ error: 'required field(s) missing' });
+      };
       const newIssue = {
         assigned_to,
         status_text,
@@ -39,16 +56,40 @@ module.exports = function (app) {
       };
       try{
         const db = getDB();
-        const result = await db.collection("issues").insertOne(newIssue);
+        const result = await db.collection(project).insertOne(newIssue);
         return res.status(201).json({_id:result.insertedId,...newIssue});
       } catch(err){
-        res.status(500).json({'error':'server error'})
+        res.status(500).json({ error: 'required field(s) missing' });
       }
     })
     
-    .put(function (req, res){
+    .put(async function (req, res){
       let project = req.params.project;
-      
+      const _id = req.body._id;
+      const updateData = {}
+      for (let key in req.body){
+        if (req.body[key] && key != "_id"){
+          //updateData[key] = {$regex: req.body[key], $options: "i"}
+          updateData[key] = req.body[key]
+          // if(key == "open" && key !== undefined){
+
+          // } else {
+
+          // }
+        }
+      }
+      console.log("updateData: ", updateData);
+      if(!_id) return res.json({ error: 'missing _id' });
+      if(!Object.keys(updateData).length) return res.json({ error: 'no update field(s) sent', '_id': _id })
+      try{
+        const db = await getDB();
+        // const issueRes = await db.collection(project).UpdateOne({_id: new ObjectId(_id)}, {$set: updateData});
+        const issueRes = await db.collection(project).find({_id: new ObjectId("67cd8f49dcf472ab031706ac")}).toArray();
+        console.log("response:",issueRes);
+        return res.json({  result: 'successfully updated', '_id': _id })
+      } catch(err){
+        return res.json({ error: 'could not update', '_id': _id });
+      }
     })
     
     .delete(function (req, res){
